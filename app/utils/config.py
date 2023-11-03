73cea09afabd0c48fb24de29c6b246992789d7ae
Benjamin Chauhan <benjamin.chauhan@duke.edu>
2023-11-03 19:33:39 -0400

    more config

--- app/utils/config.py
+++ app/utils/config.py
@@ -3,6 +3,8 @@ import os
 WEAVIATE_CLIENT_URL = ""
 WEAVIATE_API_KEY = os.environ["WEAVIATE_API_KEY"]
 
-MONGODB_URL = 'mongodb+srv://awesome:uZZOnSgXroGrSpMR@cluster0.pwoojny.mongodb.net/?retryWrites=true&w=majority'
+MONGODB_URL = 'mongodb+srv://awesome:uZZnSgXroGrSpMR@cluster0.pwoojny.mongodb.net/?retryWrites=true&w=majority'
 MONGODB_DB = 'ai-server'
 MONGODB_COLLECTION = 'prompts'
+
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
--- app/utils/weaviate_connector.py
+++ app/utils/weaviate_connector.py
@@ -1,7 +1,11 @@
 import weaviate
-
-
+from config import WEAVIATE_CLIENT_URL, WEAVIATE_API_KEY, OPENAI_API_KEY
 
 db = weaviate.Client(
-
+    url=WEAVIATE_CLIENT_URL,
+    auth_client_secret=weaviate.AuthApiKey(WEAVIATE_API_KEY),
+    additional_headers={"X-OpenAI-Api-Key": OPENAI_API_KEY},
 )
+
+
+
